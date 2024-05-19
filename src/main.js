d7d06dfbb10d1374aea9164739b566f80b87b42c
Abilov599 <abilovv599@gmail.com>
2024-05-20 00:37:53 +0400

    Fix CSRF token handling in main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const csrfTokenMetaTag = document.querySelector('meta[name="csrf-token"]');
 
-  if (!csrfTokenMetaTag) {
-    return console.error("Meta tag with name 'csrf-token' not found.");
-  }
+  if (!csrfTokenMetaTag) return;
 
   const csrfToken = csrfTokenMetaTag.getAttribute("content");
 
-  if (!csrfToken) {
-    return console.error("Meta tag with name 'csrf-token' has no content.");
-  }
+  if (!csrfToken) return;
 
   const tokenInput = document.querySelectorAll('input[name="_token"]');
 
